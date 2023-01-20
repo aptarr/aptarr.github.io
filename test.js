32399646af906a179725b950c8c8122dbe1370c4
aptarr <apta.rasendriya@gmail.com>
2023-01-21 04:49:24 +0700

    commit 5

--- test.js
+++ test.js
@@ -18,6 +18,30 @@ function parallax(e){
     });
 }
 
+const carousel = document.querySelector(".slide");
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) =>{
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+
 ScrollReveal({
     distance: '50px',
     duration: 2000,
